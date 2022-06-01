22d55f6968f011386f3ba8edce5a7b5de7acbcc3
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2022-06-01 17:08:08 +0200

    fix: fixed copyright information

--- runExperiment.py
+++ runExperiment.py
@@ -1,16 +1,16 @@
-//  Copyright 2021 Taras Lykhenko, Rafael Soares
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+#  Copyright 2021 Taras Lykhenko, Rafael Soares
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 import argparse
 
@@ -192,7 +192,7 @@ def experiment(test, experiments,redisService, redisPort, hydro, faasTCC):
             logger.info('*** Experiment system %s %s = %s ***' % (system, test, str(experiment)))
             setup[test] = experiment
             setupKubernetes(systems[system])
-            time.sleep(600)
+            time.sleep(900)
             logging.info('*** Being Populate ***')
             populate(r,p, setup["bname"],setup["num_requests"],setup["dag_name"],setup["db_size"],setup["tx_size"],setup["dag_size"],setup["zipf"], create = True)
             time.sleep(90)
